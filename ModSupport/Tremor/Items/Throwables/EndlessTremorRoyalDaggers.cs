588c28dd05dfd5d45e56a805de05162356ad9cb2
Joey van Sluijs <superjoey003@gmail.com>
2017-07-07 12:52:23 +0200

    Added partial support for tremor
    potions and alchemy weapons still missing
    
    Some items, namely the royal daggers and twilight knife aren't behaving as their original items

---
+++ ModSupport/Tremor/Items/Throwables/EndlessTremorRoyalDaggers.cs
@@ -0,0 +1,37 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Infinity.ModSupport.Tremor.Items.Throwables
+{
+    public class EndlessTremorRoyalDaggers : ModItem
+    {
+        Mod otherMod = ModLoader.GetMod("Tremor");
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Endless Royal Daggers");
+        }
+
+        public override void SetDefaults()
+        {
+            if (otherMod != null)
+            {
+                item.CloneDefaults(otherMod.ItemType("RoyalDaggers"));
+                item.consumable = false;
+                item.maxStack = 1;
+            }
+        }
+
+        public override void AddRecipes()
+        {
+            if (otherMod != null)
+            {
+                ModRecipe recipe = new ModRecipe(mod);
+                recipe.AddIngredient(otherMod.ItemType("RoyalDaggers"), 3996);
+                recipe.AddTile(TileID.WorkBenches);
+                recipe.SetResult(this);
+                recipe.AddRecipe();
+            }
+        }
+    }
+}
