87b1a34b660290c9fc0268ffae3d5f50a601cf91
Lolkat76 <devlolkat92@gmail.com>
2017-08-02 11:03:08 -0400

    Update and rename EndlessEctoplasmDart.cs to EndlessDartExpansionEctoplasmDart.cs

--- ModSupport/DartExpansion/Items/Ammo/EndlessDartExpansionEctoplasmDart.cs
+++ ModSupport/DartExpansion/Items/Ammo/EndlessDartExpansionEctoplasmDart.cs
@@ -3,7 +3,7 @@ using Terraria.ModLoader;
 
 namespace Infinity.ModSupport.DartExpansion.Items.Ammo
 {
-	public class EndlessEctoplasmDart : ModItem
+	public class EndlessDartExpansionEctoplasmDart : ModItem
 	{
         Mod otherMod = ModLoader.GetMod("DartExpansion");
 
