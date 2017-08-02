c8f0516b64c93da22e72b556c519755d7ad4746a
DragonHunter003 <superjoey003@gmail.com>
2017-08-02 23:59:43 +0200

    Feature dart expansion (#29)
    * Added Dart Expansion Support (Placeholder sprites)
    
    Currently lack sprites, using a placeholder sprite for each item right now.
    
    * Update and rename EndlessChlorophyteDart.cs to EndlessDartExpansionChlorophyteDart.cs
    
    * Update and rename EndlessEctoplasmDart.cs to EndlessDartExpansionEctoplasmDart.cs
    
    * Update and rename EndlessGlassDart.cs to EndlessDartExpansionGlassDart.cs
    
    * Update and rename EndlessGoldenDart.cs to EndlessDartExpansionGoldenDart.cs
    
    * Update and rename EndlessHellfireDart.cs to EndlessDartExpansionHellfireDart.cs
    
    * Update and rename EndlessNanoDart.cs to EndlessDartExpansionNanoDart.cs
    
    * Update and rename EndlessPartyDart.cs to EndlessDartExpansionPartyDart.cs
    
    * Update and rename EndlessSepticDart.cs to EndlessDartExpansionSepticDart.cs
    
    * Update and rename EndlessSpikeDart.cs to EndlessDartExpansionSpikeDart.cs
    
    * Update and rename EndlessUnholyDart.cs to EndlessDartExpansionUnholyDart.cs
    
    * Update and rename EndlessVenomDart.cs to EndlessDartExpansionVenomDart.cs
    
    * Update and rename EndlessVortexDart.cs to EndlessDartExpansionVortexDart.cs
    
    * Delete EndlessChlorophyteDart.png
    
    * Delete EndlessEctoplasmDart.png
    
    * Delete EndlessGlassDart.png
    
    * Delete EndlessGoldenDart.png
    
    * Delete EndlessHellfireDart.png
    
    * Delete EndlessNanoDart.png
    
    * Delete EndlessPartyDart.png
    
    * Delete EndlessSepticDart.png
    
    * Delete EndlessSpikeDart.png
    
    * Delete EndlessUnholyDart.png
    
    * Delete EndlessVenomDart.png
    
    * Delete EndlessVortexDart.png
    
    * Reuploaded placeholders with new names
    
    * Replaced placeholders with golden versions

---
+++ ModSupport/DartExpansion/Items/Ammo/EndlessDartExpansionSpikeDart.cs
@@ -0,0 +1,38 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Infinity.ModSupport.DartExpansion.Items.Ammo
+{
+	public class EndlessDartExpansionSpikeDart : ModItem
+	{
+        Mod otherMod = ModLoader.GetMod("DartExpansion");
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Endless Spike Dart");
+            Tooltip.SetDefault("Provides infinite Spike Darts");
+        }
+
+		public override void SetDefaults()
+		{
+            if (otherMod != null)
+            {
+                item.CloneDefaults(otherMod.ItemType("SpikeDart"));
+                item.consumable = false;
+                item.maxStack = 1;
+            }
+        }
+
+		public override void AddRecipes()
+		{
+            if (otherMod != null)
+            {
+                ModRecipe recipe = new ModRecipe(mod);
+                recipe.AddIngredient(otherMod.ItemType("SpikeDart"), 999);
+                recipe.AddTile(TileID.MythrilAnvil);
+                recipe.SetResult(this);
+                recipe.AddRecipe();
+            }
+		}
+	}
+}
